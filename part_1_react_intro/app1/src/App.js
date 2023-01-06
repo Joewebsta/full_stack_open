c81eaffba80bb8f7aa5556086b20c47ad2e05490
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-05 20:54:48 -0500

    Complete exercise 1.5

--- part_1_react_intro/app1/src/App.js
+++ part_1_react_intro/app1/src/App.js
@@ -4,12 +4,11 @@ const Total = ({parts}) => {
   )
 }
 
-const Header = props => {
-  return (<h1>{props.course}</h1>)
+const Header = ({course}) => {
+  return (<h1>{course}</h1>)
 }
 
 const Part = props => {
-  console.log(props.part);
   return (
     <p>
       {props.part.name} {props.part.exercises}
@@ -29,27 +28,29 @@ const Content = ({parts}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
