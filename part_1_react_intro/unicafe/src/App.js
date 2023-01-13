d9e23c37b055c6aad0ab195858b86fe8f1b86d78
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-13 07:31:05 -0500

    Complete unicafe step 2

--- part_1_react_intro/unicafe/src/App.js
+++ part_1_react_intro/unicafe/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = good + bad + neutral
+  let average = all / 3;
+  let positive = good / all * 100;
 
   const handleClickGood = () => setGood(good + 1)
   const handleClickNeutral = () => setNeutral(neutral + 1)
@@ -24,6 +27,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive}%</div>
     </>
   )
 }
