a392ea5f3817b800a2a182af2a0a40360be9b6e3
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-14 12:42:11 -0500

    Complete exercise 2.1 - Course information step 6

--- part_2_communicating_with_server/part2/src/App.js
+++ part_2_communicating_with_server/part2/src/App.js
@@ -1,15 +1,55 @@
-import React from "react"
-import Note from "./components/Note"
+import React from 'react'
 
-const App = ({ notes }) => {
+const Course = ({ course }) => {
+  if (Object.keys(course).length === 0) {
+    return <p>There are no courses.</p>
+  }
+
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
-      </ul>
-    </div>
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </>
   )
+}
+
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Content = ({ parts }) => {
+  if (parts.length === 0) {
+    return <p>There are no lessons.</p>
+  }
+
+  return parts.map(part => <Part key={part.id} part={part} />)
+}
+
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
