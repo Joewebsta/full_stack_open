0f1185eab8a6db3a7f8484c437de8b39f41fc77f
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-16 18:34:10 -0500

    Complete part2 lesson B content

--- part_2_communicating_with_server/part2-notes-part2-1/src/App.js
+++ part_2_communicating_with_server/part2-notes-part2-1/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+
+    fetch('http://localhost:3001/notes')
+      .then(response => response.json())
+      .then(notes => {
+        console.log('promise fulfilled')
+        setNotes(notes)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
