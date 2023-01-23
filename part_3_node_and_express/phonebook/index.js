58bfc348f866891dcc53dc94019d6da0f8f7cb00
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-23 18:56:55 -0500

    Complete exercises 3.2 and 3.3

--- part_3_node_and_express/phonebook/index.js
+++ part_3_node_and_express/phonebook/index.js
@@ -5,30 +5,49 @@ app.use(express.json())
 
 let persons = [
   { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
   },
   { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
   },
   { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
   },
   { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
 ]
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).json({
+      error: `Person with id ${id} cannot be found.`
+    })
+  }
+})
+
+app.get('/info', (request, response) => {
+  const info = `<p>Phonebook has info for ${persons.length} people<p><p>${new Date()}</p>`
+  response.send(info)
+})
+
 
 const PORT = 3001
 app.listen(PORT)
