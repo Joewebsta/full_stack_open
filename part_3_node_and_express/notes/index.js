8defc872f45d0ffc3789f7df8ecd05802130b8fd
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-23 16:37:05 -0500

    Complete Part 3 Section A content

--- part_3_node_and_express/notes/index.js
+++ part_3_node_and_express/notes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-const notes = [
+app.use(express.json())
+
+let notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -20,12 +22,58 @@ const notes = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Notes</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/notes', (request, response) => {
+app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404)
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) : 0
+
+  return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
