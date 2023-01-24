9c783549829f244a6dad96127e8c6b207ee78769
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-24 15:14:51 -0500

    Update PORT variable

--- part_3_node_and_express/notes/index.js
+++ part_3_node_and_express/notes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -75,7 +77,8 @@ const generateId = () => {
   return maxId + 1
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
