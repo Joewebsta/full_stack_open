d70543a0ceb3672142efec8d50201135b0265007
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-23 20:55:04 -0500

    Add two middleware functions

--- part_3_node_and_express/phonebook/index.js
+++ part_3_node_and_express/phonebook/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
   { 
@@ -90,6 +99,12 @@ const isDuplicateName = ({ name }) => {
 const response400 = (response, message) => {
   response.status(400).json({error: message})
 }
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
