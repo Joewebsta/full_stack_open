cc13a136cc8aac58eb450cbc2d13f2f66c341e57
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-26 17:18:57 -0500

    Create notes model and, create environment variables, fetch notes from mongodb server

--- part_3_node_and_express/notes2/backend/index.js
+++ part_3_node_and_express/notes2/backend/index.js
@@ -1,27 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-01-10T17:30:31.098Z",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2022-01-10T18:39:34.091Z",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-01-10T19:20:14.298Z",
+//     important: true
+//   }
+// ]
 
 app.use(express.json())
 app.use(cors())
@@ -61,7 +63,9 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -82,7 +86,7 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
