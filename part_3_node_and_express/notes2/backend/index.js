770ee13ac6dd739c6fda36f1fc900e5361d8be3e
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-26 19:00:32 -0500

    Complete exercises 3.13 and 3.14

--- part_3_node_and_express/notes2/backend/index.js
+++ part_3_node_and_express/notes2/backend/index.js
@@ -4,42 +4,13 @@ const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-01-10T17:30:31.098Z",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-01-10T18:39:34.091Z",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-01-10T19:20:14.298Z",
-//     important: true
-//   }
-// ]
-
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -50,16 +21,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes', (req, res) => {
@@ -76,14 +45,9 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const PORT = process.env.PORT
--- part_3_node_and_express/phonebook/backend/index.js
+++ part_3_node_and_express/phonebook/backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
+const Person = require('./models/phonebook')
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
@@ -10,31 +12,10 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -44,28 +25,21 @@ app.post('/api/persons', (request, response) => {
   if (!body.number) return response400(response, "Number missing")
   if (isDuplicateName(body)) return response400(response, "Name must be unique")
 
-  const person = {
-    id: generateID(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).json({
-      error: `Person with id ${id} cannot be found.`
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
     })
-  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -85,13 +59,8 @@ app.get('/info', (request, response) => {
   response.send(info)
 })
 
-const generateID = () => {
-  return Math.round(Math.random() * 999_999_999 + 1);
-}
-
 const isDuplicateName = ({ name }) => {
-  const namesArr = persons.map(person => person.name)
-  return namesArr.includes(name)
+  return Person.find({ "name": name }).length > 0
 }
 
 const response400 = (response, message) => {
@@ -105,6 +74,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
