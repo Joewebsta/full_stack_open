bef8a1146fdd67fe06122af23823e522de4756c7
Joe Webster <joseph.scott.webster@gmail.com>
2023-01-28 12:48:12 -0500

    Update eslint configuration

--- part_3_node_and_express/notes2/backend/index.js
+++ part_3_node_and_express/notes2/backend/index.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config()
+const express = require("express")
+
 const app = express()
-const cors = require('cors')
-const Note = require('./models/note')
+const cors = require("cors")
+const Note = require("./models/note")
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
   }
 
-  if (error.name === 'ValidationError') {
+  if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
   }
 
@@ -19,24 +20,24 @@ const errorHandler = (error, request, response, next) => {
 }
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: "unknown endpoint" })
 }
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static("build"))
 
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
     res.json(notes)
   })
 })
 
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+app.post("/api/notes", (request, response, next) => {
+  const { body } = request
 
   if (!body.content) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: "content missing" })
   }
 
   const note = new Note({
@@ -44,46 +45,46 @@ app.post('/api/notes', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
+  note.save().then((savedNote) => {
     response.json(savedNote)
   })
-  .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-app.get('/api/notes/:id', async (request, response, next) => {
+app.get("/api/notes/:id", async (request, response, next) => {
   Note.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.put("/api/notes/:id", (request, response, next) => {
   const { content, important } = request.body
 
   Note.findByIdAndUpdate(
-    request.params.id, 
+    request.params.id,
     { content, important },
-    { new: true, runValidators: true, context: 'query' }
+    { new: true, runValidators: true, context: "query" },
   )
-    .then(updatedNote => response.json(updatedNote))
-    .catch(error => next(error))
+    .then((updatedNote) => response.json(updatedNote))
+    .catch((error) => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
     .then(() => response.status(204).end())
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const { PORT } = process.env
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part_3_node_and_express/notes2/backend/models/note.js
+++ part_3_node_and_express/notes2/backend/models/note.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config()
+const mongoose = require("mongoose")
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+console.log("connecting to", url)
 
 mongoose.connect(url)
   .then(() => {
-    console.log('connected to MongoDB');
+    console.log("connected to MongoDB")
   })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message)
   })
 
 const noteSchema = new mongoose.Schema({
@@ -24,12 +24,12 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema)
