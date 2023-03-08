318a69b261940a7a98d2275dafbc6b7222bcfd60
Joe Webster <joseph.scott.webster@gmail.com>
2023-03-08 11:37:50 -0500

    Install bcrypt

--- part_3_node_and_express/notes2/backend/tests/note_api.test.js
+++ part_3_node_and_express/notes2/backend/tests/note_api.test.js
@@ -9,8 +9,7 @@ const Note = require('../models/note')
 beforeEach(async () => {
   await Note.deleteMany({})
 
-  const noteObjects = helper.initialNotes
-    .map(note => new Note(note))
+  const noteObjects = helper.initialNotes.map(note => new Note(note))
   const promiseArray = noteObjects.map(note => note.save())
   await Promise.all(promiseArray)
 })
