760039de893fabdbd154e5d6433b3e557a9982ad
Joe Webster <joseph.scott.webster@gmail.com>
2023-03-09 14:47:44 -0500

    Conditionally display login form or notes form depending on whether a user is logged in

--- part_3_node_and_express/notes2/frontend/src/App.js
+++ part_3_node_and_express/notes2/frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -22,13 +26,6 @@ const App = () => {
       important: Math.random() < 0.5,
     }
 
-    // const response = await fetch('http://localhost:3001/notes', {
-    //   method: 'POST',
-    //   headers: {'Content-Type': 'application/json'},
-    //   body: JSON.stringify(noteObject)
-    // })
-    // const note = await response.json();
-
     const note = await noteService.create(noteObject)
     setNotes(notes.concat(note))
     setNewNote('')
@@ -39,7 +36,6 @@ const App = () => {
   }
 
   const toggleImportanceOf = async id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
@@ -51,30 +47,88 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      // setErrorMessage('Wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+    }
+    console.log('logging in with', username, password);
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  )
+
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note 
-            key={note.id} 
-            note={note}
-            toggleImportance = {() => toggleImportanceOf(note.id)}
-          />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
+      <>
+        <h1>Notes</h1>
+
+        {/* <Notification message={errorMessage} /> */}
+
+        {!user && loginForm()}
+        {user && <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+        }
+
+        <h2>Notes</h2>
+
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note =>
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+      </>
     </div>
   )
 }
