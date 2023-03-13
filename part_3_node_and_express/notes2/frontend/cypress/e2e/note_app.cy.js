d8092fb1454fac2d9ff0e953b8cd84f86790ea19
Joe Webster <joseph.scott.webster@gmail.com>
2023-03-13 16:39:37 -0400

    Create testingRouter and cypress tests for toggling note importance

--- part_3_node_and_express/notes2/frontend/cypress/e2e/note_app.cy.js
+++ part_3_node_and_express/notes2/frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe('Note app', function () {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Joe Webster',
+      username: 'joewebster',
+      password: 'nike'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -34,5 +41,23 @@ describe('Note app', function () {
       cy.contains('save').click()
       cy.contains('a note created by cypress')
     })
+
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made not important', function () {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make important')
+      })
+    })
+
   })
 })
