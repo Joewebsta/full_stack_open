98341625c2c0106b85aee2167e2ff894c27afeea
Joe Webster <joseph.scott.webster@gmail.com>
2023-03-13 19:53:20 -0400

    Create blog login form

--- part_4_testing_express/blog2/frontend/src/App.js
+++ part_4_testing_express/blog2/frontend/src/App.js
@@ -1,19 +1,35 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  // const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h1>Blogs</h1>
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
