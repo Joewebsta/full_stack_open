304b5b6643c21ad6ba7cba7dbf191dfd0830a7c0
Joe Webster <joseph.scott.webster@gmail.com>
2023-03-14 19:29:40 -0400

    Create NewNote and Notes components

--- part_6_advanced_state_management/notes-simplified/src/App.js
+++ part_6_advanced_state_management/notes-simplified/src/App.js
@@ -1,56 +1,12 @@
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
-
-
-const Note = ({ content, important, handleClick }) => {
-  const status = important ? 'important' : ''
-
-  return (
-    <li onClick={handleClick}>
-      {content} <strong>{status}</strong>
-    </li>
-  )
-}
-
-const Notes = ({ notes, handleClick }) => {
-  return (
-    <ul>
-      {notes.map(note => (
-        <Note
-          key={note.id}
-          content={note.content}
-          important={note.important}
-          handleClick={() => handleClick(note.id)}
-        />)
-      )}
-    </ul>
-  )
-}
+import Notes from "./components/Notes"
+import NewNote from "./components/NewNote"
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const addNote = (e) => {
-    e.preventDefault()
-
-    const content = e.target.note.value
-    e.target.note.value = ''
-    dispatch(createNote(content))
-  }
-
-  const toggleImportance = id => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
       <h1>Notes</h1>
-      <form onSubmit={addNote}>
-        <input name="note" type="text" />
-        <button type="submit">Add</button>
-      </form>
-      <Notes notes={notes} handleClick={toggleImportance} />
+      <NewNote />
+      <Notes />
     </div>
   )
 }
--- part_6_advanced_state_management/notes-simplified/src/reducers/noteReducer.js
+++ part_6_advanced_state_management/notes-simplified/src/reducers/noteReducer.js
@@ -2,12 +2,16 @@ const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload]
-    case 'TOGGLE_IMPORTANCE': {
+    case 'TOGGLE_IMPORTANCE':
       const id = action.payload.id
-      const noteToChange = state.find(note => note.id === id)
-      const changedNote = { ...noteToChange, important: !noteToChange.important }
-      return state.map(note => note.id !== id ? note : changedNote)
-    }
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote
+      )
     default:
       return state
   }
