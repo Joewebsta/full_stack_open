3c3c916bb3fa8c1703a5abe2520b678b4f6a1594
Joe Webster <joseph.scott.webster@gmail.com>
2023-06-26 21:17:02 -0400

    Initial commit

---
+++ part_1_react_intro/unicafe_3/src/App.test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { describe, expect, it } from "vitest";
+import { screen, render } from "@testing-library/react";
+import App from "./App";
+
+it("Test", () => {
+  expect(1).toBe(1);
+});
