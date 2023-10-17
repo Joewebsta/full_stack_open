d7055c2ffec47a498d2bd727e3fdc50306cfa8f3
Joe Webster <joseph.scott.webster@gmail.com>
2023-10-16 20:07:41 -0400

    Complete exercise 9.7

--- part_9_typescript/exercises/src/calculateExercises.ts
+++ part_9_typescript/exercises/src/calculateExercises.ts
@@ -2,21 +2,22 @@ import { createRating, createRatingDescription } from "./utils/rating";
 import {
   countNonZeroExerciseDays,
   calculateAverageDailyExerciseHours,
-  parseArguments,
 } from "./utils/utils";
 
 import { Result } from "./types";
 
-function calculateExercises(args: string[]): Result {
-  if (args.length <= 2) {
+export function calculateExercises(
+  dailyExerciseHours: number[],
+  target: number
+): Result {
+  if (dailyExerciseHours.length <= 2) {
     throw new Error("Please provide daily target and daily exercise hours.");
   }
 
-  if (args.length === 3) {
+  if (dailyExerciseHours.length === 3) {
     throw new Error("Please provide daily exercise hours.");
   }
 
-  const [target, ...dailyExerciseHours] = parseArguments(args);
   const periodLength = dailyExerciseHours.length;
   const trainingDays = countNonZeroExerciseDays(dailyExerciseHours);
   const average = calculateAverageDailyExerciseHours(dailyExerciseHours);
@@ -33,12 +34,4 @@ function calculateExercises(args: string[]): Result {
     rating,
     ratingDescription,
   };
-}
-
-try {
-  console.log(calculateExercises(process.argv));
-} catch (error: unknown) {
-  if (error instanceof Error) {
-    console.log(`Error: ${error.message}`);
-  }
 }
--- part_9_typescript/exercises/src/index.ts
+++ part_9_typescript/exercises/src/index.ts
@@ -1,18 +1,45 @@
 import express from "express";
-import calculateBmi from "../archive/calculateBmi";
+// import { calculator, Operation } from "./calculator";
+import { calculateExercises } from "./calculateExercises";
 const app = express();
+app.use(express.json());
 const PORT = 3003;
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
 });
 
-app.get("/bmi", (req, res) => {
-  const {height, weight} = req.query;
-  
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { dailyExerciseHours, target } = req.body;
 
-  res.json();
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculateExercises(dailyExerciseHours, target);
+    res.send(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send({ error: error.message });
+    }
+  }
 });
+
+// app.post("/calculate", (req, res) => {
+//   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//   const { value1, value2, op } = req.body;
+
+//   if (!value1 || isNaN(Number(value1))) {
+//     return res.status(400).send({ error: "..." });
+//   }
+
+//   if (!value2 || isNaN(Number(value2))) {
+//     return res.status(400).send({ error: "..." });
+//   }
+
+//   const result = calculator(Number(value1), Number(value2), op as Operation);
+
+//   res.send({ result });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
