1652619c1da776a5bcd24361acf54f8fceadd4ae
Joe Webster <joseph.scott.webster@gmail.com>
2023-10-18 21:02:12 -0400

    Complete exercise 9.9

--- part_9_typescript/patientor2/server/src/index.ts
+++ part_9_typescript/patientor2/server/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on port ${3000}`);
+app.listen(3001, () => {
+  console.log(`Server running on port ${3001}`);
 });
