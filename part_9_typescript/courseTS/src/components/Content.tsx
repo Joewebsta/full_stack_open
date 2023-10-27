524277c15a4afb240d2b983c48d8e4bf669a62a0
Joe Webster <joseph.scott.webster@gmail.com>
2023-10-27 14:02:34 -0400

    Complete exercise 9.15

--- part_9_typescript/courseTS/src/components/Content.tsx
+++ part_9_typescript/courseTS/src/components/Content.tsx
@@ -1,17 +1,71 @@
-import { ContentProps } from "../types";
+import { ContentProps, PartProps } from "../types";
 
 export const Content = (props: ContentProps) => {
   return (
     <>
-      <p>
-        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
-      </p>
+      {props.courseParts.map((coursePart, index) => {
+        return <Part key={index} coursePart={coursePart} />;
+      })}
     </>
+  );
+};
+
+const Part = (props: PartProps) => {
+  switch (props.coursePart.kind) {
+    case "basic":
+      return (
+        <div>
+          <p style={{ marginBottom: 0 }}>
+            <b>{`${props.coursePart.name} ${props.coursePart.exerciseCount}`}</b>
+          </p>
+          <p style={{ marginTop: 0 }}>
+            <em>{props.coursePart.description}</em>
+          </p>
+        </div>
+      );
+    case "background":
+      return (
+        <div>
+          <p style={{ marginBottom: 0 }}>
+            <b>{`${props.coursePart.name} ${props.coursePart.exerciseCount}`}</b>
+          </p>
+          <p style={{ margin: 0 }}>
+            <em>{props.coursePart.description}</em>
+          </p>
+          <p style={{ marginTop: 0 }}>
+            {`Submit to ${props.coursePart.backgroundMaterial}`}
+          </p>
+        </div>
+      );
+    case "group":
+      return (
+        <div>
+          <p style={{ marginBottom: 0 }}>
+            <b>{`${props.coursePart.name} ${props.coursePart.exerciseCount}`}</b>
+          </p>
+          <p style={{ marginTop: 0 }}>
+            {`project exercises ${props.coursePart.groupProjectCount}`}
+          </p>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+          <p style={{ marginBottom: 0 }}>
+            <b>{`${props.coursePart.name} ${props.coursePart.exerciseCount}`}</b>
+          </p>
+          <p style={{ marginTop: 0 }}>
+            {`project exercises: ${props.coursePart.requirements.join(", ")}`}
+          </p>
+        </div>
+      );
+    default:
+      return assertNever(props.coursePart);
+  }
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
   );
 };
--- part_9_typescript/courseTS/src/types.ts
+++ part_9_typescript/courseTS/src/types.ts
@@ -2,15 +2,67 @@ export interface HeaderProps {
   courseName: string;
 }
 
-interface Course {
-  name: string;
-  exerciseCount: number;
+export interface ContentProps {
+  courseParts: CoursePart[];
 }
 
-export interface ContentProps {
-  courseParts: Course[];
+export interface PartProps {
+  coursePart: CoursePart;
 }
 
 export interface TotalProps {
   totalExercises: number;
 }
+
+// name
+// exerciseCount
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+// name
+// exerciseCount
+// groupProjectCount
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+// name
+// exerciseCount
+// description
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+// name
+// exerciseCount
+// description
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+// name
+// exerciseCount
+// description
+// backgroundMaterial
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+// name
+// exerciseCount
+// description
+// requirements
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial;
