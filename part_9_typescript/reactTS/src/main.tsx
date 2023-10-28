a9b635eb3f8327518f5830a54269061f091dbea9
Joe Webster <joseph.scott.webster@gmail.com>
2023-10-28 10:21:15 -0400

    ReactTS - Communicate with the server

--- part_9_typescript/reactTS/src/main.tsx
+++ part_9_typescript/reactTS/src/main.tsx
@@ -1,16 +1,6 @@
 // import React from 'react'
 import ReactDOM from "react-dom/client";
-// import App from './App.tsx'
+import App from "./App.tsx";
 import "./index.css";
 
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps) => {
-  return <h1>Hello, {props.name}</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Welcome name="Joe" />
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
